b6179f200ae18b9085763d03686bc5370deca175
Chris Eddy <ceddy.zen@gmail.com>
2019-03-04 17:29:19 -0800

    update calendar dates

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -5,7 +5,7 @@ import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 import * as firebase from 'firebase';
 
-import myEventsList from '../events.js';
+import events from '../events.js';
 
 import Navbar from './navbar.jsx';
 import Footer from './footer.jsx';
@@ -19,29 +19,6 @@ class Calendar extends Component {
     this.state = {
         events: []
     };
-    this.loadEvents = this.loadEvents.bind(this);
-}
-
-  componentDidMount(){
-    console.log(myEventsList[0])
-    this.loadEvents();
-  }
-
-  loadEvents(){
-    const db = firebase.firestore();
-    var docRef = db.collection("data").doc("3dxcxftejADMxR9xFLJq");
-    console.log('grabbing events')
-
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            const events = doc.data().json;
-            console.log(JSON.stringify(events, null, 2));
-        } else {
-            console.log("No such document!");
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
 }
 
   render() {
@@ -51,7 +28,7 @@ class Calendar extends Component {
         <div id="calendarContainer">
           <BigCalendar
             localizer={localizer}
-            events={this.state.events}
+            events={events}
             startAccessor="start"
             endAccessor="end"
           />
--- src/events.js
+++ src/events.js
@@ -1,9 +1,240 @@
 export default [
     {
       id: 0,
-      title: 'All Day Event very long title',
+      title: 'AGM',
       allDay: true,
-      start: '2019, 2, 2',
-      end: '2019, 2, 3'
+      start: '2019, 4, 9',
+      end: '2019, 4, 9'
+    },
+    {
+      id: 1,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 3, 12',
+      end: '2019, 3, 12'
+    },
+    {
+      id: 2,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 4, 9',
+      end: '2019, 4, 9'
+    },
+    {
+      id: 3,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 5, 14',
+      end: '2019, 5, 14'
+    },
+    {
+      id: 4,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 6, 11',
+      end: '2019, 6, 11'
+    },
+    {
+      id: 5,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 7, 9',
+      end: '2019, 7, 9'
+    },
+    {
+      id: 6,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 8, 13',
+      end: '2019, 8, 13'
+    },
+    {
+      id: 7,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 9, 10',
+      end: '2019, 9, 10'
+    },
+    {
+      id: 8,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 10, 8',
+      end: '2019, 10, 8'
+    },
+    {
+      id: 9,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 11, 12',
+      end: '2019, 11, 12'
+    },
+    {
+      id: 10,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2019, 12, 10',
+      end: '2019, 12, 10'
+    },
+    {
+      id: 11,
+      title: 'Society Meeting',
+      allDay: true,
+      start: '2020, 1, 14',
+      end: '2020, 1, 14'
+    },
+    {
+      id: 12,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 3, 7',
+      end: '2019, 3, 7'
+    },
+    {
+      id: 13,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 3, 21',
+      end: '2019, 3, 21'
+    },
+    {
+      id: 14,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 4, 4',
+      end: '2019, 4, 4'
+    },
+    {
+      id: 15,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 4, 18',
+      end: '2019, 4, 18'
+    },
+    {
+      id: 16,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 5, 2',
+      end: '2019, 5, 2'
+    },
+    {
+      id: 17,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 5, 16',
+      end: '2019, 5, 16'
+    },
+    {
+      id: 18,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 6, 6',
+      end: '2019, 6, 6'
+    },
+    {
+      id: 19,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 6, 20',
+      end: '2019, 6, 20'
+    },
+    {
+      id: 20,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 7, 4',
+      end: '2019, 7, 4'
+    },
+    {
+      id: 21,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 7, 18',
+      end: '2019, 7, 18'
+    },
+    {
+      id: 22,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 8, 1',
+      end: '2019, 8, 1'
+    },
+    {
+      id: 23,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 8, 15',
+      end: '2019, 8, 15'
+    },
+    {
+      id: 24,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 9, 5',
+      end: '2019, 9, 5'
+    },
+    {
+      id: 25,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 9, 19',
+      end: '2019, 9, 19'
+    },
+    {
+      id: 26,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 10, 3',
+      end: '2019, 10, 3'
+    },
+    {
+      id: 27,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 10, 17',
+      end: '2019, 10, 17'
+    },
+    {
+      id: 28,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 11, 7',
+      end: '2019, 11, 7'
+    },
+    {
+      id: 29,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 11, 21',
+      end: '2019, 11, 21'
+    },
+    {
+      id: 30,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 12, 5',
+      end: '2019, 12, 5'
+    },
+    {
+      id: 31,
+      title: 'Training',
+      allDay: true,
+      start: '2019, 12, 19',
+      end: '2019, 12, 19'
+    },
+    {
+      id: 32,
+      title: 'Training',
+      allDay: true,
+      start: '2020, 1, 2',
+      end: '2020, 1, 2'
+    },
+    {
+      id: 33,
+      title: 'Training',
+      allDay: true,
+      start: '2020, 1, 16',
+      end: '2020, 1, 16'
     }
   ]
